4e0aeac5e3b577b2877c11198e4b544c10bfcf21
zeonaraa <rpsmaster12@gmail.com>
2025-02-14 10:49:41 +0700

    feat: initialize repository

---
+++ src/lib/dexie.ts
@@ -0,0 +1,22 @@
+import Dexie, { Table } from "dexie";
+
+// Table declaration
+interface DEX_Thread {
+    id: string;
+    title: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+class ChatDB extends Dexie {
+    threads!: Table<DEX_Thread>;
+    constructor() {
+        super("chatdb")
+
+        this.version(1).stores({
+            threads: "id, title, created_at, updated_at"
+        })
+
+        this.threads.hook("creating")
+    }
+}
